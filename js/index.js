8599d7b6a6b017c1b5a5f0de2a52c340fadbd6e9
EmreKurt <ekurt@live.unc.edu>
2016-12-01 19:19:52 -0500

    fixed login
    fiex

--- js/index.js
+++ js/index.js
@@ -2,5 +2,11 @@ function login(){
   var email = $("#email").val();
   var password = $("#password").val();
   console.log(email + password);
-  return false;
+
+  $.get( "http://152.23.118.214/cats/server_side/login.php?email=" + email + "&password=" + password, function( data ) {
+  $( ".result" ).html( data );
+  console.log(data);
+  console.log(JSON.parse(data));
+});
+  return false; 
 }
